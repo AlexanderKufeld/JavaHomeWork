83c6f97675a325877d50efe1d2f4d3c02c48f8fe
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-04-08 18:51:54 +0200

    error handling

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,37 +1,32 @@
-
+import static java.lang.String.valueOf;
 
 public class HomeWork1 {
 
 
     // my spim esli ne rab. den' ili vyhodnoy, ostal'noe vremja net
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation) {
-            return true;
-        }
-        return false;
 
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return (!weekday || vacation);
     }
 
     //zavodim peremennuju dif; esli n>21 umnozhaem na 2
     public int diff21(int n) {
-        int dif = 21 - n;
+        int dif;
+        dif = 21 - n;
         if (n > 21) {
-            dif = Math.abs(n) * 2; // dif=(n-21)*2;
+            dif = (n - 21) * 2;
         }
         return dif;
     }
 //Дана строка​ , вернуть новую строку, где последние 3 символа в верхнем регистре (заглавные). Если строка длины меньше 3, перевести это в заглавные.
 
     public String endUp(String str) {
-        int beginUpper = str.length() - 3;
-        int length = str.length();
-        if (str.length() < 3)
+        if (str.length() >= 3) {
+            String Upperstr = str.substring(str.length() - 3, str.length());
+            return str.substring(0, str.length() - 3) + Upperstr.toUpperCase();
+        } else
             return str.toUpperCase();
-        String upper = str.substring(beginUpper).toUpperCase();
-        return upper;
-
     }
-
 
     //Даны три целых числы​ a b c, вернуть наибольшее.
     public int intMax(int a, int b, int c) {
@@ -41,83 +36,92 @@ public class HomeWork1 {
         } else {
             max = b;
 
-            if (c > a && c > b)
+            if (c > a && c > b) {
                 max = c;
-            return max;
+            }
+        }
+        return max;
+    }
 
-            //return (a>b&& a>c)? a: (b>a&& b>c)? b : c;
+
+        //Даны два массива целых чисел.​ Вернуть true если у них одинаковый первый или последний элемент. Оба массива длины 1 или более.
+
+        public boolean commonEnd ( int[] arrayA, int[] arrayB){
+            return arrayA[0] == arrayB[0] || arrayA[arrayA.length - 1] == arrayB[arrayB.length - 1];
         }
 
 
-    //Даны два массива целых чисел.​ Вернуть true если у них одинаковый первый или последний элемент. Оба массива длины 1 или более.
-    public boolean commonEnd (int[] a,  int [] b) {
-           if  (a[0] == b[0] || a[a.length - 1] == b[b.length - 1])
-            return true;
-    }
 //Дан массив целых чисел.​ Вернуть сумму двух первых элементов массива. Если длина массива меньше двух, вернуть сумму всех элементов. Вернуть 0, если длина массива равна нулю.
 
-    public int sum2(int[] nums) {
-        int sum = nums[0] + nums[nums.length - 1];
-        return sum;
-        // ne ponjal gde oshibka:
-        // return (nums.length < 2) ? (nums[0] + nums.length - 1) : (nums[0] + (nums[nums.length - 1]));
-    }
+        public int sum2 (int[] nums){
+        int sum=0;
+            if (nums.length >= 2)
+                return (nums[0] + nums[nums.length-1]);
+            if (nums.length < 1)
+                return 0;
+
+            return sum;
+        }
+
 //Вернуть количество четных чисел в массиве​ целых чисел. Напомню, что оператор % "mod" вычисляет остаток от деления, e.g. 5 % 2 равно 1.
 
-    public int countEvens(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0)
-                count++;
+        public int countEvens ( int[] numbers){
+            int count;
+            count = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] % 2 == 0)
+                    count++;
+            }
+            return count;
         }
-        return count;
-    }
 //Вернуть версию входного массива​ , где каждыое нулевое значение заменено максимальным нечетным значением, справа от нуля. Если нечетных числе справа от нуля нет, то оставляем ноль.
 
-    public int[] zeroMax(int[] nums) {
+        public int[] zero ( int[] numb){
             int max;
-            for(int i = 0; i < nums.length - 1; i++) {
-                if(nums[i] == 0) {
+            for (int i = 0; i < numb.length - 1; i++) {
+                if (numb[i] == 0) {
                     max = 0;
-                    for(int j = i + 1; j < nums.length; j--) {
-                        if(nums[j] > max && nums[j] % 2 == 1)
-                            max = nums[j];
+                    for (int j = i + 1; numb.length > j; j--) {
+                        if (numb[j] > max && numb[j] % 2 == 1)
+                            max = numb[j];
                     }
-                    if(max != 0)
-                        nums[i] = max;
+                    if (max != 0)
+                        numb[i] = max;
                 }
             }
-            return nums;
-
+            return numb;
         }
 //Вернуть массив, ​ смещенный влево на один индекс.
 
-    public int[] shiftLeft(int[] nums){
+        public int shiftLeft (int[] nums){
             int temp = nums[0];
-            if(nums.length <= 1)
-                return  nums;
-
-            for(int i = 0; i < nums.length - 1; i++) {
-                nums[i] = nums[i + 1];
+            if (nums.length <= 1) {
+                int temp1 = temp;
+                return temp1;
             }
-            nums[nums.length - 1] = temp;
-            return nums;
-    }
 
-    //Даны две строки,​ вернуть их конкатенацию, но без первого символа в каждой. Строки ненулевой длины.
+            for (int i = 0; i < nums.length - 1; i++)
+                nums[i] = nums[i + 1];
 
-    public String nonStart(String a;  String  b) {
-            int i = a + b.charAt(1, 1);
+            temp = nums[nums.length - 1];
+            return temp;
+        }
+
+        //Даны две строки,​ вернуть их конкатенацию, но без первого символа в каждой. Строки ненулевой длины.
+
+        public String nonStart (String a, String b){
+            String i= valueOf(a.charAt(1)+b.charAt(1));
             return i;
 
-    }
+        }
 //Дана строка четной длины.​ Вернуть строку из двух центральных символов, например строка "string" превратиться в "ri". Длина входной строки минимум 2.
-public String middleTwo (String str) {
-        int length = str.length();
-        int middle = length / 2;
-        return str.substring(middle -1, middle + 1);
-    }
+        public String middleTwo (String str){
+
+            int length = str.length();
+            int middle = length / 2;
+            return str.substring(middle - 1, middle + 1);
+        }
+
+
 
 }
-
-
--- src/HomeWork1Test.java
+++ src/HomeWork1Test.java
@@ -3,33 +3,66 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HomeWork1Test {
+
+    // test dlja sleepIn
     @Test
-
-    public void testSleepIn{
-
-        public boolean sleepIn1 (boolean weekday, boolean vacation) {
-           sleepIn s = new sleepIn ();
-            boolean vacation = s.sleepIn();
-            assertTrue (vacation);
-
+    public void testSleepIn1() {
+        HomeWork1 hw = new HomeWork1();
+        boolean b = hw.sleepIn(false, false);
+        assertTrue(b);
     }
 
+    @Test
+    public void testSleepIn2() {
+        HomeWork1 hw = new HomeWork1();
+        boolean b = hw.sleepIn(true, false);
+        assertFalse(b);
+    }
 
+    @Test
+    public void testSleepIn3() {
+        HomeWork1 hw = new HomeWork1();
+        boolean b = hw.sleepIn(false, true);
+        assertTrue(b);
+    }
 
+    //test dlja diff21
+    @Test
+    public void testDiff21(int n) {
+        HomeWork1 t = new HomeWork1();
+        int dif = t.diff21(11);
+        assertEquals(dif, t);
+    }
 
+    @Test
+    public void testDiff21_1(int n) {
+        HomeWork1 t = new HomeWork1();
+        int dif = t.diff21(21);
+        assertEquals(dif, t);
+    }
 
+    @Test
+    public void testDiff21_2(int n) {
+        HomeWork1 t = new HomeWork1();
+        int dif = t.diff21(31);
+        assertEquals(dif, t);
+    }
 
+    //test: Дана строка​ , вернуть новую строку, где последние 3 символа в верхнем регистре (заглавные). Если строка длины меньше 3, перевести это в заглавные.
+@Test
+    public void testEndUp(String str) {
+        //   endUp("Hello") → "HeLLO"
+        //   endUp("hi there") →"hi thERE"
+        //   endUp("hi") →HI"
 
+        HomeWork1 s = new HomeWork1();
+        String s1= "Hello";
+        String s2="hi there";
+        String s3="hi";
+        s.endUp(s1);
+        s.endUp(s2);
+        s.endUp(s3);
+        assertEquals(str,s);
 
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
