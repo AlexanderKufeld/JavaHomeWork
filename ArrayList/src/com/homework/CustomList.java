c8469b95757c80f84fb8289841e1362a0f2e56c4
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-04-27 17:15:01 +0200

    first solution

---
+++ ArrayList/src/com/homework/CustomList.java
@@ -0,0 +1,16 @@
+package com.homework;
+
+public interface CustomList<E> {
+
+        int size();
+
+        void append(E o);
+
+        E get(int index);
+
+        E removeById(int index);
+
+        boolean removeByElement(E e);
+    }
+
+
